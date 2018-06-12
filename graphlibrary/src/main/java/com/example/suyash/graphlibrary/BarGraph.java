8ddd0e9e2a5575b732f2cb331d2a7e6abe8aa89f
suyash <jsuyash1514@gmail.com>
2018-06-12 23:33:34 +0530

    Minor changes

--- graphlibrary/src/main/java/com/example/suyash/graphlibrary/BarGraph.java
+++ graphlibrary/src/main/java/com/example/suyash/graphlibrary/BarGraph.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+
 
 import java.util.ArrayList;
 import java.util.List;
