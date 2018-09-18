7b3930229b375788247952daa25811e75e882bad
jsuyash1514 <jsuyash1514@gmail.com>
2018-09-19 03:12:02 +0530

    Completed layout of piechart

--- app/src/main/java/com/example/suyash/graph/PieChartAddEntry.java
+++ app/src/main/java/com/example/suyash/graph/PieChartAddEntry.java
@@ -1,7 +1,10 @@
 package com.example.suyash.graph;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PieChartAddEntry extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class PieChartAddEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pie_chart_add_entry);
+
+        ImageButton close = findViewById(R.id.pieEntryCloseButton);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this,PieChartNew.class));
+        finish();
     }
 }
