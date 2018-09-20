13251c77fb797f1d6f6c89e491e78948b92c68a2
jsuyash1514 <jsuyash1514@gmail.com>
2018-09-20 15:06:42 +0530

    Add colorizer to pieChart

--- app/src/main/java/com/example/suyash/graph/PieChartAddEntry.java
+++ app/src/main/java/com/example/suyash/graph/PieChartAddEntry.java
@@ -1,12 +1,18 @@
 package com.example.suyash.graph;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+
 public class PieChartAddEntry extends AppCompatActivity {
+    int selectedColorR, selectedColorG, selectedColorB, selectedColorRGB;
+    ImageButton DarkBlue, LightBlue, Red, Yellow, Green, Grey, pieChartSelectedcolor, colorize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,96 @@ public class PieChartAddEntry extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        DarkBlue = findViewById(R.id.DarkBlue);
+        LightBlue = findViewById(R.id.LightBlue);
+        Red = findViewById(R.id.Red);
+        Yellow = findViewById(R.id.yellow);
+        Green = findViewById(R.id.green);
+        Grey = findViewById(R.id.grey);
+        pieChartSelectedcolor = findViewById(R.id.pieChartSelectedColor);
+        colorize = findViewById(R.id.pieChartColorizer);
+
+        selectedColorRGB = getResources().getColor(R.color.grey);``
+
+        DarkBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.darkBlue);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        LightBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.lightBlue);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        Red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.red);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        Yellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.yellow);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        Green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.green);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        Grey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedColorRGB = getResources().getColor(R.color.grey);
+                pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+            }
+        });
+
+        final Context context = this;
+
+        final ColorPicker cp = new ColorPicker(this);
+
+        colorize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cp.show();
+                Button okColor = (Button) cp.findViewById(R.id.okColorButton);
+
+                okColor.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectedColorR = cp.getRed();
+                        selectedColorG = cp.getGreen();
+                        selectedColorB = cp.getBlue();
+                        selectedColorRGB = cp.getColor();
+                        pieChartSelectedcolor.setBackgroundColor(selectedColorRGB);
+                        cp.dismiss();
+                    }
+                });
+            }
+        });
+
+
     }
 
     @Override
-    public void onBackPressed(){
-        startActivity(new Intent(this,PieChartNew.class));
+    public void onBackPressed() {
+        startActivity(new Intent(this, PieChartNew.class));
         finish();
     }
 }
